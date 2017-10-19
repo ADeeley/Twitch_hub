762aee9ffd0f9dfdc7b11aa0b81a22f76703bc81
Adam M Deeley <ad.deeley@gmail.com>
2017-10-19 22:12:52 +0100

    Added a dynamic user list with logos and usernames populated from JSON

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 var URL = "https://wind-bow.gomix.me/twitch-api";
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-function doSomething(data) {
-    console.log(data[0]);
-}
+$(document).ready( function() {
+    // Populate the list of users in the HTML 
+    for (var user = 0; user < users.length; user++) {
+        
+        // Get the JSONP and harvest the details for each user-block
+        $.getJSON(URL + '/users/' + users[user] + '?callback=?', function(data) {
+            var userListEl = document.createElement("li");
+            
+            // Get logo from logoURL
+            var logo = document.createElement("img");
+            logo.src = data['logo'];
+            logo.style = "height:50px; width:50px";
 
-window.onload = function() {
-    $.getJSON(URL + '/users/FreeCodeCamp?callback=?', function(data) {
-        var image = document.createElement("img");
-        image.src = data['logo'];
-        image.style = "width:50px; height: 50px;";
-        document.getElementById('FreeCodeCamp:logo').appendChild(image);
-    });
-
-}
+            // Create username
+            var userName = document.createElement("p")
+            userName.innerHTML = data['name'];
+            userListEl.appendChild(logo);
+            userListEl.appendChild(userName);
+            document.getElementsByTagName("ul")[0].appendChild(userListEl);
+        });
+    }
+});
 
 
 
