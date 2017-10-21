db434071e52940b1b17d5a7aee5a52a9b0c01384
Adam M Deeley <ad.deeley@gmail.com>
2017-10-21 11:51:34 +0100

    Added online status and tagline by pulling from json streams and channels.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var URL = "https://wind-bow.gomix.me/twitch-api";
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 $(document).ready( function() {
+    var twitchURL = "https://go.twitch.tv";
     // Populate the list of users in the HTML 
     for (var user = 0; user < users.length; user++) {
         
@@ -11,15 +12,41 @@ $(document).ready( function() {
             
             // Get logo from logoURL
             var logo = document.createElement("img");
-            logo.src = data['logo'];
-            logo.style = "height:50px; width:50px";
+            logo.src       = data['logo'];
+            logo.style     = "height:50px; width:50px";
+            logo.classList = "logo";
 
             // Create username
-            var userName = document.createElement("p")
+            var userName = document.createElement("a")
             userName.innerHTML = data['name'];
-            userListEl.appendChild(logo);
-            userListEl.appendChild(userName);
-            document.getElementsByTagName("ul")[0].appendChild(userListEl);
+            userName.classList = "userLink";
+            userName.href      = twitchURL + '/' + data['name'];
+
+            // Get status tagline
+            $.getJSON(URL + '/channels/' + data['name'] + '?callback=?', function(channelData) {
+
+                var status = document.createElement("p");
+                status.style = "font-size: 16px; margin: 0 0 0 50%;";
+                status.innerHTML = channelData['status'];
+                
+                //Get online status
+                $.getJSON(URL + '/streams/' + data['name'] + '?callback=?', function(streamData) {
+                var onlineStatus = document.createElement("p");
+                    if (!streamData['stream']) {
+                        onlineStatus.innerHTML = "Offline";
+                    }
+                    else {
+                        onlineStatus.innerHTML = "Online Now";
+                    }
+                    // Add everything to the list element
+                    userListEl.appendChild(logo);
+                    userListEl.appendChild(userName);
+                    userListEl.appendChild(status);
+                    userListEl.appendChild(onlineStatus);
+
+                    document.getElementsByTagName("ul")[0].appendChild(userListEl);
+                });
+            });
         });
     }
 });
