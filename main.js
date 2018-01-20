a0f5aaba97e6b31eaf0b597895c62fdae1277d5c
Adam Deeley <ad.deeley@gmail.com>
2018-01-20 16:20:42 +0000

    Moved the JS source to the bottom of the HTML body in order to remove the on  window load function from the JS.

--- main.js
+++ main.js
@@ -1,57 +1,56 @@
-$(document).ready( function() {
-    var URL = "https://wind-bow.gomix.me/twitch-api";
-    var twitchURL = "https://go.twitch.tv";
-    var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
-        
-    var cellArr = [];
-    // Populate the table of channels in the HTML 
-    for (var user = 0; user < users.length; user++) {
-        let u = users[user];
-        // Get the JSONP and harvest the details for each channel 
-        $.getJSON(URL + "/users/" + u + "?callback=?", function(userData) {
-            $.getJSON(URL + "/channels/" + u + "?callback=?", function(channelData) {
-                $.getJSON(URL + "/streams/" + u + "?callback=?", function(streamData) {
-                    // Get logo from logoURL
-                    logo = document.createElement("img");
-                    logo.src       = userData["logo"];
-                    logo.classList = "logo";
+var URL = "https://wind-bow.gomix.me/twitch-api";
+var twitchURL = "https://go.twitch.tv";
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    
+var cellArr = [];
+// Populate the table of channels in the HTML 
+for (var user = 0; user < users.length; user++) {
+    let u = users[user];
+    // Get the JSONP and harvest the details for each channel 
+    $.getJSON(URL + "/users/" + u + "?callback=?", function(userData) {
+        $.getJSON(URL + "/channels/" + u + "?callback=?", function(channelData) {
+            $.getJSON(URL + "/streams/" + u + "?callback=?", function(streamData) {
+                // Get logo from logoURL
+                logo = document.createElement("img");
+                logo.src       = userData["logo"];
+                logo.classList = "logo";
 
-                    // Create username
-                    let name = userData['name'];
-                    userName = document.createElement("a")
-                    userName.innerHTML = name;
-                    userName.classList = "userLink";
-                    userName.href      = twitchURL + "/" + name;
+                // Create username
+                let name = userData['name'];
+                userName = document.createElement("a")
+                userName.innerHTML = name;
+                userName.classList = "userLink";
+                userName.href      = twitchURL + "/" + name;
 
-                    // Get status tagline
-                    userStatus = document.createElement("p");
-                    userStatus.innerHTML = channelData["status"];
-                    
-                    //Get online status
-                    isOnline = document.createElement("p");
-                    if (!streamData['stream']) {
-                        isOnline.innerHTML = "Offline";
-                    }
-                    else {
-                        isOnline.innerHTML = "Online Now";
-                    }
-                    // Define a new row
-                    let row = document.getElementById("channels").insertRow();
-                    // Add cells to the row
-                    cellArr = [logo, userName, userStatus, isOnline];
+                // Get status tagline
+                userStatus = document.createElement("p");
+                userStatus.innerHTML = channelData["status"];
+                
+                //Get online status
+                isOnline = document.createElement("p");
+                if (!streamData['stream']) {
+                    isOnline.innerHTML = "Offline";
+                }
+                else {
+                    isOnline.innerHTML = "Online Now";
+                }
+                // Define a new row
+                let row = document.getElementById("channels").insertRow();
+                // Add cells to the row
+                cellArr = [logo, userName, userStatus, isOnline];
 
-                    cellArr.forEach(function(item, index) {
-                        cell = row.insertCell(index);
-                        cell.appendChild(item);
-                    });
-                    var h = document.getElementsByTagName("h3")[0].style = "color: white";
-                    // Remove the loading placeholder when channels are displayed
-                    document.getElementById("loadText").style.display = "none";
+                cellArr.forEach(function(item, index) {
+                    cell = row.insertCell(index);
+                    cell.appendChild(item);
                 });
+                var h = document.getElementsByTagName("h3")[0].style = "color: white";
+                // Remove the loading placeholder when channels are displayed
+                document.getElementById("loadText").style.display = "none";
             });
         });
-    }
-});
+    });
+}
+
 function showAll() {
     /**
      * Shows all users
