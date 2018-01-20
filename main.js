2c64145669a86f00439bd82beb962155926f0f85
Adam Deeley <ad.deeley@gmail.com>
2018-01-20 16:40:34 +0000

    Replaced vars with lets and moved variable declarations to the top of functions for clarity.

--- main.js
+++ main.js
@@ -1,63 +1,66 @@
-var URL = "https://wind-bow.gomix.me/twitch-api";
-var twitchURL = "https://go.twitch.tv";
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
-    
-var cellArr = [];
+const URL = "https://wind-bow.gomix.me/twitch-api";
+const twitchURL = "https://go.twitch.tv";
+const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
 // Populate the table of channels in the HTML 
-for (var user = 0; user < users.length; user++) {
-    let u = users[user];
-    // Get the JSONP and harvest the details for each channel 
-    $.getJSON(URL + "/users/" + u + "?callback=?", function(userData) {
-        $.getJSON(URL + "/channels/" + u + "?callback=?", function(channelData) {
-            $.getJSON(URL + "/streams/" + u + "?callback=?", function(streamData) {
-                // Get logo from logoURL
-                logo = document.createElement("img");
-                logo.src       = userData["logo"];
-                logo.classList = "logo";
+function populateTable() {
+    let cellArr = [];
 
-                // Create username
-                let name = userData['name'];
-                userName = document.createElement("a")
-                userName.innerHTML = name;
-                userName.classList = "userLink";
-                userName.href      = twitchURL + "/" + name;
+    for (var user = 0; user < users.length; user++) {
+        let u = users[user];
+        // Get the JSONP and harvest the details for each channel 
+        $.getJSON(URL + "/users/" + u + "?callback=?", function(userData) {
+            $.getJSON(URL + "/channels/" + u + "?callback=?", function(channelData) {
+                $.getJSON(URL + "/streams/" + u + "?callback=?", function(streamData) {
+                    let row = document.getElementById("channels").insertRow(),
+                        name = userData['name'],
+                        h = document.getElementsByTagName("h3")[0].style = "color: white";
+                    // Get logo from logoURL
+                    logo = document.createElement("img");
+                    logo.src       = userData["logo"];
+                    logo.classList = "logo";
 
-                // Get status tagline
-                userStatus = document.createElement("p");
-                userStatus.innerHTML = channelData["status"];
-                
-                //Get online status
-                isOnline = document.createElement("p");
-                if (!streamData['stream']) {
-                    isOnline.innerHTML = "Offline";
-                }
-                else {
-                    isOnline.innerHTML = "Online Now";
-                }
-                // Define a new row
-                let row = document.getElementById("channels").insertRow();
-                // Add cells to the row
-                cellArr = [logo, userName, userStatus, isOnline];
+                    // Create username
+                    userName = document.createElement("a")
+                    userName.innerHTML = name;
+                    userName.classList = "userLink";
+                    userName.href      = twitchURL + "/" + name;
 
-                cellArr.forEach(function(item, index) {
-                    cell = row.insertCell(index);
-                    cell.appendChild(item);
+                    // Get status tagline
+                    userStatus = document.createElement("p");
+                    userStatus.innerHTML = channelData["status"];
+                    
+                    //Get online status
+                    isOnline = document.createElement("p");
+                    if (!streamData['stream']) {
+                        isOnline.innerHTML = "Offline";
+                    }
+                    else {
+                        isOnline.innerHTML = "Online Now";
+                    }
+                    // Add cells to the row
+                    cellArr = [logo, userName, userStatus, isOnline];
+
+                    cellArr.forEach(function(item, index) {
+                        cell = row.insertCell(index);
+                        cell.appendChild(item);
+                    });
+                    // Remove the loading placeholder when channels are displayed
+                    document.getElementById("loadText").style.display = "none";
                 });
-                var h = document.getElementsByTagName("h3")[0].style = "color: white";
-                // Remove the loading placeholder when channels are displayed
-                document.getElementById("loadText").style.display = "none";
             });
         });
-    });
+    }
 }
 
 function showAll() {
     /**
      * Shows all users
      */
-    var isOnline;
-    var rows = document.getElementById("channels").rows;
-    for (var i = 1; i < rows.length; i++) {
+    let isOnline = undefined,
+        rows = document.getElementById("channels").rows,
+        i = 1;
+    for (i; i < rows.length; i++) {
             rows[i].style.display = "";
 
     }
@@ -68,9 +71,9 @@ function showOnline() {
      * Hides non-online users
      */
     showAll();
-    var isOnline;
-    var rows = document.getElementById("channels").rows;
-    for (var i = 1; i < rows.length; i++) {
+    let isOnline = undefined,
+        rows = document.getElementById("channels").rows;
+    for (i; i < rows.length; i++) {
         isOnline = rows[i].cells[3].getElementsByTagName("p")[0].innerHTML;
         if (isOnline == "Offline") {
             rows[i].style.display = "none";
@@ -84,10 +87,11 @@ function showOffline() {
      * Hides online users
      */
     showAll();
-    console.log("Clicked  show Offline");
-    var isOnline;
-    var rows = document.getElementById("channels").rows;
-    for (var i = 1; i < rows.length; i++) {
+    let isOnline,
+        rows = document.getElementById("channels").rows,
+        i = 1;
+
+    for (i; i < rows.length; i++) {
         isOnline = rows[i].cells[3].getElementsByTagName("p")[0].innerHTML;
         if (isOnline == "Online Now") {
             rows[i].style.display = "none";
@@ -96,24 +100,4 @@ function showOffline() {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+populateTable();
